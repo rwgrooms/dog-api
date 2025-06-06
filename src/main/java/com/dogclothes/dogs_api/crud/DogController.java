d02a9b3f6c498a0d9cc38e41f9dc9aac31b78972
Robert Grooms <rwgrooms@uncg.edu>
2025-06-06 15:52:59 -0400

    initial udpate to add views

--- src/main/java/com/dogclothes/dogs_api/crud/DogController.java
+++ src/main/java/com/dogclothes/dogs_api/crud/DogController.java
@@ -3,26 +3,42 @@ package com.dogclothes.dogs_api.crud;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.stereotype.Controller;
 import java.util.List;
 
-@RestController
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
 @RequestMapping("/api/dogs")
 public class DogController {
 
     @Autowired
     private DogService dogService;
 
-    @GetMapping
-    public List<Dog> getAllDogs() {
-        return dogService.getAllDogs();
+    @GetMapping()
+    public Object getAllDogs(Model model) {
+        model.addAttribute("dogsList" , dogService.getAllDogs());
+        model.addAttribute("title", "All Dogs");
+        return "animal-list";
+        //return dogService.getAllDogs();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Dog> getDogById(@PathVariable Long id) {
-        return dogService.getDogById(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    public Object getDogById(@PathVariable Long id, Model model) {
+        model.addAttribute("dogsL" , dogService.getDogById(id));
+        model.addAttribute("title", "Dog #: " + id);
+        return "animal-details";
+        //return dogService.getDogById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
     @GetMapping("/breed/{breed}")
